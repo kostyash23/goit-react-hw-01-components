0de68e39b80cd45ad3572ce9d7e92652354938e4
kostyash23 <kostiashmotoloha@gmail.com>
2019-09-19 16:47:02 +0300

    repos

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import Profile from './componens/Profile/Profile';
+// import img from './componens/Profile/avatar.png';
+
+// const user = {
+//   name: 'Timothy Grant',
+//   tag: '@tgrant',
+//   location: 'Sao Paulo, Brazil',
+//   avatar: img,
+//   stats: {
+//     followers: 198,
+//     views: 3685,
+//     likes: 492,
+//   },
+// };
+// function App() {
+//   return <Profile />;
+// }
+
+// export default App;
---
+++ src/componens/Profile/Profile.js
@@ -0,0 +1,36 @@
+// import React from 'react';
+// import './Profile.css';
+
+// const Profile = ({ user }) => {
+//   return (
+//     <div className="profile">
+//       <div class="description">
+//         <img
+//           src="https://i.pinimg.com/originals/a0/40/66/a04066a2d1fcf25df39c599e093995c8.jpg"
+//           alt="user avatar"
+//           class="avatar"
+//         />
+//         <p class="name">Petra Marica</p>
+//         <p class="tag">@pmarica</p>
+//         <p class="location">Salvador, Brasil</p>
+//       </div>
+
+//       <ul class="stats">
+//         <li>
+//           <span class="label">Followers</span>
+//           <span class="quantity">1000</span>
+//         </li>
+//         <li>
+//           <span class="label">Views</span>
+//           <span class="quantity">2000</span>
+//         </li>
+//         <li>
+//           <span class="label">Likes</span>
+//           <span class="quantity">3000</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Profile;
